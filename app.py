c732a4dc71b93b46878453ddef9f685aae97a9d8
UncleVanyaKaleka <137908775+UncleVanyaKaleka@users.noreply.github.com>
2023-09-21 16:13:54 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,43 @@
+import telebot
+from config import keys, TOKEN
+from Utils import ConvertionException, CurrencyConverter
+
+
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=["start", "help"])
+def help(message):
+    bot.send_message(message.chat.id, f"Приветствую, {message.from_user.first_name} {message.from_user.last_name}. \
+\nДля начала работы требуется ввести :  \n<Валюту которая у вас есть> \
+\n<Валюту в которую вы хотите перевести деньги> \
+\n<Количество переводимой валюты>\
+\n Для просмотра доступных валют введите команду /values ")
+
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = "Доступные Валюты: "
+    for key in keys.keys():
+        text = '\n'.join((text, key, ))
+    bot.reply_to(message, text)
+
+@bot.message_handler(content_types=["text", ])
+def convert(message: telebot.types.Message):
+    try:
+        values = message.text.split(" ")
+
+        if len(values) != 3:
+            raise ConvertionException("Слишком много значений")
+
+        quote, base, amount = values
+        total_base = CurrencyConverter(quote, base, amount)
+    except ConvertionException as e:
+        bot.reply_to(message, f"Ошибка пользователя \n{e}")
+    except Exception as e:
+        bot.reply_to(message, f'Не удалось обработать команду\n{e}')
+    else:
+        text = f'Цена {amount} {quote} в {base} - {total_base}'
+        bot.send_message(message.chat.id, text)
+
+
+bot.polling()
---
+++ config.py
@@ -0,0 +1,7 @@
+keys = {
+    "Доллар": "USD",
+    "Евро": "EUR",
+    "Рубль": "RUB",
+}
+
+TOKEN = "6595557994:AAEckLvUUauAmQ5tULcVzpCxfdnxLrYBiEA"
